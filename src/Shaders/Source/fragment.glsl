06740cdbab3c4d2b44ba1f9c315327ada289eccc
remo285 <fran9910@gmail.com>
2024-12-29 16:26:53 -0300

    small fix to vertex and fragment

--- src/Shaders/Source/fragment.glsl
+++ src/Shaders/Source/fragment.glsl
@@ -9,5 +9,5 @@ uniform sampler2D texture0;
 uniform sampler2D texture1;
 
 void main() {
-    color = mix(texture(texture0, texCoords), texture(texture1, vec2(1.0 - texCoords.x, texCoords.y)), 0.2);
+    color = mix(texture(texture0, texCoords), texture(texture1, texCoords), 0.2);
 }
--- src/Source.cpp
+++ src/Source.cpp
@@ -40,10 +40,10 @@ int main(void)
 
     float positions[] = {
         // position         // color           // texture coordinates
-        0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,   // top right
-        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,   // bottom right
-       -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,   // bottom left
-       -0.5f,  0.5f, 0.0f,  0.5f, 0.5f, 0.5f,  0.0f, 1.0f,   // top left 
+        0.5f,  0.5f, 1.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 1.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,   // bottom right
+       -0.5f, -0.5f, 1.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,   // bottom left
+       -0.5f,  0.5f, 1.0f,  0.5f, 0.5f, 0.5f,  0.0f, 1.0f,   // top left 
     };
 
     unsigned int indices[] = {
